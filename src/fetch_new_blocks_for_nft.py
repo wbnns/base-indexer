1fe625a2a1b8062223e088e6856693bf3a0a3196
wbnns <will.binns@coinbase.com>
2023-11-26 09:07:52 -0600

    fetch_new_blocks_for_nft: Add script

--- src/fetch_new_blocks_for_nft.py
+++ src/fetch_new_blocks_for_nft.py
@@ -1,35 +1,54 @@
 from dotenv import load_dotenv
 import os
+import psycopg2
 from web3 import Web3
+from datetime import datetime
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Retrieve the API key
+# Database and Alchemy connection details
+db_connection_string = os.getenv('DATABASE_URL')
 alchemy_api_key = os.getenv('ALCHEMY_API_KEY')
-if not alchemy_api_key:
-    raise ValueError("Alchemy API key not found in .env file.")
 
-alchemy_url = f"https://base-mainnet.g.alchemy.com/v2/{alchemy_api_key}"
+# Connect to the database
+conn = psycopg2.connect(db_connection_string)
+cur = conn.cursor()
+
+# Test database connection
+def test_db_connection():
+    try:
+        cur.execute("SELECT NOW();")
+        db_time = cur.fetchone()
+        print(f"Database connection successful. Current database time: {db_time[0]}")
+    except Exception as e:
+        print(f"Database connection test failed: {e}")
+
+test_db_connection()
 
 # Connect to Base using Alchemy
-w3 = Web3(Web3.HTTPProvider(alchemy_url))
+w3 = Web3(Web3.HTTPProvider(f"https://base-mainnet.g.alchemy.com/v2/{alchemy_api_key}"))
 
-# Ensure connection is established
 if not w3.is_connected():
     raise ConnectionError("Failed to connect to Base blockchain.")
 
-# Function to process a block
+# Function to process a block and store contract creations
 def process_block(block_number):
     block = w3.eth.get_block(block_number, full_transactions=True)
     print(f"Processing Block: {block['number']}")
     for tx in block['transactions']:
-        # Check for contract creation (to address is None)
         if tx['to'] is None:
-            print(f"Potential NFT Contract Creation: {tx['hash'].hex()}")
-            # Here, you can add logic to store this transaction in your database
+            cur.execute(
+                "INSERT INTO contract_creations (tx_hash, block_number, timestamp, contract_address) VALUES (%s, %s, %s, %s) ON CONFLICT (tx_hash) DO NOTHING",
+                (tx['hash'].hex(), block['number'], datetime.utcfromtimestamp(block['timestamp']), None)
+            )
+            conn.commit()
 
 # Fetch and process the latest block
 latest_block_number = w3.eth.block_number
 process_block(latest_block_number)
+
+# Close the database connection
+cur.close()
+conn.close()
 
